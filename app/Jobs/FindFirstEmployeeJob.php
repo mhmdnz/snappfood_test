5c9697f458f37f35b370a51048ed32b82a741b94
vagrant <vagrant@homestead>
2020-05-11 15:03:15 +0000

    add Jobs

---
+++ app/Jobs/FindFirstEmployeeJob.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Helper\AnswerLogHelper;
+use App\Models\Employee;
+use App\Models\IncomingCall;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class FindFirstEmployeeJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $incomingCall;
+
+    /**
+     * FindFirstEmployeeJob constructor.
+     * @param IncomingCall $incomingCall
+     */
+    public function __construct(IncomingCall $incomingCall)
+    {
+        $this->incomingCall = $incomingCall;
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function handle()
+    {
+        $waited_employee = Employee::getFirstWaited();
+        if ($waited_employee)
+            AnswerLogHelper::save($this->incomingCall, $waited_employee);
+
+        throw new \Exception('waited for new User');
+    }
+}
